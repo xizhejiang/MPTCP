fdbeb6e390c07992e51b595e1fb22c1a50e4fd7c
Xizhe Jiang <xizhe_jiang@qq.com>
2016-08-20 19:42:00 +0100

    script with protocal

--- script-tool/batch_ln.sh
+++ script-tool/batch_ln.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-target=$1
-total=$2
+#tcp = 0, mptcp = 1
+protocal=$1%2
+echo $protocal
+target=$2
+total=$3
 current_folder=`pwd`
 echo $current_folder
-for ((i=0;i<$total;i++)) 
+
+for ((i=$protocal;i<$total;i=i+2)) 
 do
     mkdir $i
     ln_target=$current_folder"/"$i
@@ -13,4 +17,61 @@ done
 
 
 
+
+#!/bin/bash
+#tcp = 0, mptcp = 1
+protocal=$1%2
+echo $protocal
+target=$2
+total=$3
+current_folder=`pwd`
+echo $current_folder
+
+for ((i=$protocal;i<$total;i=i+2)) 
+do
+    mkdir $i
+    ln_target=$current_folder"/"$i
+    ln -s $current_folder"/"$i $current_folder"/"$target
+    #echo $current_folder
+done
+
+
+
+
+#!/bin/bash
+#tcp = 0, mptcp = 1
+protocal=$1%2
+echo $protocal
+target=$2
+total=$3
+current_folder=`pwd`
+echo $current_folder
+
+for ((i=$protocal;i<$total;i=i+2)) 
+do
+    mkdir $i
+    ln_target=$current_folder"/"$i
+    ln -s $current_folder"/"$i $current_folder"/"$target
+    #echo $current_folder
+done
+
+
+
+
+#!/bin/bash
+#tcp = 0, mptcp = 1
+protocal=$1%2
+echo $protocal
+target=$2
+total=$3
+current_folder=`pwd`
+echo $current_folder
+
+for ((i=$protocal;i<$total;i=i+2)) 
+do
+    mkdir $i
+    ln_target=$current_folder"/"$i
+    ln -s $current_folder"/"$i $current_folder"/"$target
+    #echo $current_folder
+done
 
