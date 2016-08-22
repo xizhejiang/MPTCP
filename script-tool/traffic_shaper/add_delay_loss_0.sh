e0f9e78d3657740dc55dfede2bdebe2efb54eed1
imink <patrick.wang1029@gmail.com>
2016-08-22 15:40:48 +0100

    add delay loss script

---
+++ script-tool/traffic_shaper/add_delay_loss_0.sh
@@ -0,0 +1,38 @@
+#!/bin/bash 
+
+DEV=wlan0
+DELAY=200ms
+
+LOSS=10%
+
+
+if [ "$1" = "loss" ]
+then
+    tc qdisc add dev $DEV root netem loss $LOSS
+    exit
+fi
+
+if [ "$1" = "delay" ]
+then
+    tc qdisc add dev $DEV root netem delay $DELAY
+    exit
+fi
+
+
+if [ "$1" = "status" ]
+then
+    tc -s qdisc ls dev $DEV
+    tc -s class ls dev $DEV
+    exit
+fi
+
+
+if [ "$1" = "stop" ] 
+then
+    # clean existing down- and uplink qdiscs, hide errors
+    tc qdisc del dev $DEV root    
+    tc -s qdisc ls dev $DEV
+    exit
+fi
+
+
