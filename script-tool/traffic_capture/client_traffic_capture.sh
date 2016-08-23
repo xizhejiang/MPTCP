f5d81af9f963b34367e80a8b366ee54abe01c103
imink <patrick.wang1029@gmail.com>
2016-08-23 09:14:49 -0400

    fix

--- script-tool/traffic_capture/client_traffic_capture.sh
+++ script-tool/traffic_capture/client_traffic_capture.sh
@@ -7,14 +7,16 @@ flag="ESTABLISHED"
 #flag="SYN_SENT"
 counter=1
 trigger=0
-limit=50
+limit=56
 test_name="test.pcap"
-
+key="x"
 
 output_file=${counter}.${test_name}
 #once start the script, start tcpdump
 tcpdump -i any -s 0 -w $output_file & 
 counter=$((counter+1))
+
+
 
 while [ $counter -lt $limit ]
 do 
@@ -23,28 +25,27 @@ do
     output_file=${counter}.${test_name}
     echo $command
     #set the trigger
-    if [ $command -gt 0 ] && [ $trigger -eq 0 ]; then
+    if [ $command -gt 1 ] && [ $trigger -eq 0 ]; then
         echo "setup"
         #tcpdump -i any -s 0 -w $output_file & 
         trigger=1
         #counter=$((counter+1))
     fi
 
-    if [ $command -eq 0 ] && [ $trigger -eq 1 ]; then
+    if [ $command -lt 2 ] && [ $trigger -eq 1 ]; then
         echo "close"
         #kill previous tcpdump process
-        sleep 1
+	#sleep 1
         killall tcpdump
         #start a new tcpdump
         tcpdump -i any -s 0 -w $output_file & 
         counter=$((counter+1))
         trigger=0
     fi
-
+	
     #set the interval to be 0.1s
-    sleep 0.1
+    sleep 0.01
 done
-
 
 
 
