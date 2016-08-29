96686a65bbae6a981a1edff75aa8285723cae790
Patrick <patrick.wang@gmail.com>
2016-08-29 14:21:17 +0100

    router tc

---
+++ script-tool/traffic_shaper/router_tc.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+WLAN0=192.168.1.3
+WLAN1=192.168.1.4
+DEV=eth0
+
+echo "reset interface qdisc"
+tc qdisc del dev $DEV root
+
+echo "install HTB qdisc"
+tc qdisc add dev $DEV root handle 1: htb default 10
+
+echo "root class"
+tc class add dev $DEV parent 1: classid 1:1 htb rate 102400kbit
+
+echo "LAN traffic"
+#tc class add dev $DEV parent 1:1 classid 1:10 htb rate 102400kbit
+#tc filter add dev $DEV parent 1:0 protocol ip prio 1 u32 match ip src 192.168.1.0/24 flowid 1:10
+
+echo "wlan0 traffic"
+tc class add dev $DEV parent 1:1 classid 1:11 htb rate 500kbps ceil 600kbps
+tc filter add dev $DEV parent 1:0 protocol ip prio 1 u32 match ip dst $WLAN0 flowid 1:11
+
+echo "wlan1 traffic"
+tc class add dev $DEV parent 1:1 classid 1:12 htb rate 400kbps ceil 500bps
+tc filter add dev $DEV parent 1:0 protocol ip prio 1 u32 match ip dst $WLAN1 flowid 1:12
+
+echo "other devices traffic"
+tc class add dev $DEV parent 1:1 classid 1:10 htb rate 175kbps ceil 475kbps
+
+echo "enable SFQ on leaves"
+tc qdisc add dev $DEV parent 1:10 handle 20: sfq perturb 60
+tc qdisc add dev $DEV parent 1:11 handle 21: sfq perturb 60
+tc qdisc add dev $DEV parent 1:12 handle 22: sfq perturb 60
