c10ff43c35f25972fe7b77bb899bd13c7fc34bf6
imink <patrick.wang1029@gmail.com>
2016-08-31 02:00:45 +0100

    fix

--- script-tool/traffic_shaper/router_tc.sh
+++ script-tool/traffic_shaper/router_tc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 WLAN0=192.168.1.3
 WLAN1=192.168.1.4
-DEV=eth0
+DEV=wlan0
 
 echo "reset interface qdisc"
 tc qdisc del dev $DEV root
@@ -17,17 +17,19 @@ echo "LAN traffic"
 #tc filter add dev $DEV parent 1:0 protocol ip prio 1 u32 match ip src 192.168.1.0/24 flowid 1:10
 
 echo "wlan0 traffic"
-tc class add dev $DEV parent 1:1 classid 1:11 htb rate 500kbps ceil 600kbps
+tc class add dev $DEV parent 1:1 classid 1:11 htb rate 1000kbps ceil 1200kbps
+#tc qdisc add dev $DEV parent 1:11 handle 10: netem loss 10%
 tc filter add dev $DEV parent 1:0 protocol ip prio 1 u32 match ip dst $WLAN0 flowid 1:11
 
 echo "wlan1 traffic"
-tc class add dev $DEV parent 1:1 classid 1:12 htb rate 400kbps ceil 500bps
+tc class add dev $DEV parent 1:1 classid 1:12 htb rate 1000kbps ceil 1200kbps
+#tc qdisc add dev $DEV parent 1:12 handle 20: netem loss 10%
 tc filter add dev $DEV parent 1:0 protocol ip prio 1 u32 match ip dst $WLAN1 flowid 1:12
 
 echo "other devices traffic"
-tc class add dev $DEV parent 1:1 classid 1:10 htb rate 175kbps ceil 475kbps
+tc class add dev $DEV parent 1:1 classid 1:10 htb rate 1000kbps ceil 1000kbps
 
 echo "enable SFQ on leaves"
-tc qdisc add dev $DEV parent 1:10 handle 20: sfq perturb 60
-tc qdisc add dev $DEV parent 1:11 handle 21: sfq perturb 60
-tc qdisc add dev $DEV parent 1:12 handle 22: sfq perturb 60
+#tc qdisc add dev $DEV parent 1:10 handle 20: sfq perturb 60
+#tc qdisc add dev $DEV parent 1:11 handle 21: sfq perturb 60
+#tc qdisc add dev $DEV parent 1:12 handle 22: sfq perturb 60
